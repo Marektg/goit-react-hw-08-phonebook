9bbdb8eddf2466801fb9aed2b325ddc37faa0a75
Marektg <93349057+Marektg@users.noreply.github.com>
2022-10-15 20:26:54 +0000

    Deploying to gh-pages from @ Marektg/goit-react-hw-08-phonebook@f7b9e4321f6cbcdf88b0e180e802d87535d20e41 🚀

--- static/js/445.18c75990.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_07_phonebook=self.webpackChunkgoit_react_hw_07_phonebook||[]).push([[445],{6445:function(e,n,t){t.r(n);t(2791);var s=t(9647),o=t(1590),c=t(706),h=t(3329);n.default=function(){return(0,h.jsxs)(h.Fragment,{children:[(0,h.jsx)(s.Z,{}),(0,h.jsx)("h2",{children:"Contacts"}),(0,h.jsx)(o.Z,{}),(0,h.jsx)(c.C,{})]})}}}]);
-//# sourceMappingURL=445.18c75990.chunk.js.map
